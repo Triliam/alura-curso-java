7a5968dad41943048fe1c6b47756173dbeb21962
Taís Bueno Vidotto <100791472+Triliam@users.noreply.github.com>
2022-09-21 11:19:33 -0300

    Update TestaLacos.java

--- src/javaJREeJDK/TestaLacos.java
+++ src/javaJREeJDK/TestaLacos.java
@@ -8,13 +8,12 @@ public class TestaLacos {
 
         int multiplica = 0;
 
-        for (int i = 1; i <= 10; i++){
-            for (int j = 0; j <=10; j++){
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j <=10; j++) {
                 multiplica = i * j;
                 System.out.println(multiplica);
             }
             System.out.println();
         }
-
     }
 }
