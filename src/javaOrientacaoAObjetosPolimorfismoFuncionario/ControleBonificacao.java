268658e762aec979278835134163e3be50fb83c9
Taís Bueno Vidotto <100791472+Triliam@users.noreply.github.com>
2022-09-21 11:09:22 -0300

    Update ControleBonificacao.java

--- src/javaOrientacaoAObjetosPolimorfismoFuncionario/ControleBonificacao.java
+++ src/javaOrientacaoAObjetosPolimorfismoFuncionario/ControleBonificacao.java
@@ -4,13 +4,12 @@ public class ControleBonificacao {
 
     private double soma;
 
-    public void registra(Funcionario f){
+    public void registra(Funcionario f) {
         double boni = f.getBonificacao();
         this.soma = this.soma + boni;
     }
 
-    public double getSoma(){
+    public double getSoma() {
         return soma;
     }
-
 }
