b45bda6ee4621491d80d0e80acda0a4cc4df842d
Taís Bueno Vidotto <100791472+Triliam@users.noreply.github.com>
2022-09-21 11:17:57 -0300

    Update SeguroDeVida.java

--- src/javaOrientacaoAObjetosPolimorfismoConta/SeguroDeVida.java
+++ src/javaOrientacaoAObjetosPolimorfismoConta/SeguroDeVida.java
@@ -1,6 +1,7 @@
 package javaOrientacaoAObjetosPolimorfismoConta;
 
-public class SeguroDeVida implements Tributavel{
+public class SeguroDeVida implements Tributavel {
+    
     @Override
     public double getValorImposto() {
         return 42;
