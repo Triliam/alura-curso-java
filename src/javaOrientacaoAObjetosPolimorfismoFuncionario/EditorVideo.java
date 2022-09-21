24ae6ab4f23c7955a5b2914a386a65a26336ef9b
Taís Bueno Vidotto <100791472+Triliam@users.noreply.github.com>
2022-09-21 11:10:19 -0300

    Update EditorVideo.java

--- src/javaOrientacaoAObjetosPolimorfismoFuncionario/EditorVideo.java
+++ src/javaOrientacaoAObjetosPolimorfismoFuncionario/EditorVideo.java
@@ -1,6 +1,7 @@
 package javaOrientacaoAObjetosPolimorfismoFuncionario;
 
-public class EditorVideo extends Funcionario{
+public class EditorVideo extends Funcionario {
+    
     @Override
     public double getBonificacao() {
         System.out.println("Chamando o método de bonificacao do Editor de video");
