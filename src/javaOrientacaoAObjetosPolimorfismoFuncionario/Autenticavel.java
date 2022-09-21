0647faf1612ac2e647fe357baa0f1cd369b485d0
Taís Bueno Vidotto <100791472+Triliam@users.noreply.github.com>
2022-09-21 11:07:48 -0300

    Update Autenticavel.java

--- src/javaOrientacaoAObjetosPolimorfismoFuncionario/Autenticavel.java
+++ src/javaOrientacaoAObjetosPolimorfismoFuncionario/Autenticavel.java
@@ -4,16 +4,12 @@ package javaOrientacaoAObjetosPolimorfismoFuncionario;
 //quem assina esse contrato, precisa implementar
 //metodo setSenha
 //metodo autentica
-//interface eh abstrata e oublica e seus metodos tbm, fica em cinza "abstract" e "public"
+//interface eh abstrata e publica, e seus metodos tbm, fica em cinza "abstract" e "public"
 
 public abstract interface Autenticavel {
 
-//    public abstract  void setSenha(int senha);
-//
-//    public abstract boolean autentica(int senha);
+    public abstract void setSenha(int senha);
 
-    public abstract void setSenha(int senha) ;
-
-    public abstract boolean autentica(int senha) ;
+    public abstract boolean autentica(int senha);
 }
 
