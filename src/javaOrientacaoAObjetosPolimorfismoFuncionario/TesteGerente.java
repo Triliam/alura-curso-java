0a38c5db1a55506a06a7b93b2eb70df5482ea4cb
Taís Bueno Vidotto <100791472+Triliam@users.noreply.github.com>
2022-09-21 11:14:03 -0300

    Update TesteGerente.java

--- src/javaOrientacaoAObjetosPolimorfismoFuncionario/TesteGerente.java
+++ src/javaOrientacaoAObjetosPolimorfismoFuncionario/TesteGerente.java
@@ -3,26 +3,6 @@ package javaOrientacaoAObjetosPolimorfismoFuncionario;
 public class TesteGerente {
     public static void main(String[] args) {
 
-//        Autenticavel referencia = new Cliente();
-//
-//        Gerente gerente1 = new Gerente();
-//        gerente1.setNome("Tomas");
-//        gerente1.setSalario(2.300);
-//        gerente1.getBonificacao();
-//
-////        gerente1.setSenha(2222);
-////        boolean autenticou = gerente1.autentica(2222);
-//
-//        Gerente g1 = new Gerente();
-//
-//        g1.setSenha(2222);
-//        boolean autenticacao = g1.autentica(2222);
-//
-//        System.out.println(autenticacao);
-//
-//        System.out.println(gerente1.getBonificacao());
-//      //  System.out.println(autenticou);
-
         Gerente g1 = new Gerente();
         g1.setNome("Marco");
         g1.setCpf("235568413");
@@ -34,11 +14,8 @@ public class TesteGerente {
 
         g1.setSenha(2222);
         boolean autenticou = g1.autentica(2222);
-
         System.out.println(autenticou);
 
         System.out.println(g1.getBonificacao());
-
     }
-
 }
